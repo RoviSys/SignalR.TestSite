be7a6603796d4994718e31a8465294bb74e00205
Stephen Austin <stephen.austin@rovisys.com>
2015-11-04 14:41:57 -0500

    initial commit

---
+++ SignalR.TestSite/Hubs/TesterHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.TestSite.Hubs
+{
+    public class TesterHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ SignalR.TestSite/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR.TestSite.Startup))]
+
+namespace SignalR.TestSite
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
